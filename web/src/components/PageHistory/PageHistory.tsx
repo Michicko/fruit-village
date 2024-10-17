b94f813b2277d8064869a8b10520190b0e9f54a1
Michicko <53019881+Michicko@users.noreply.github.com>
2024-10-17 22:40:18 +0100

    fixed PageHistory
    made PageHistory dynamic, to get current link
    from the url

--- web/src/components/PageHistory/PageHistory.tsx
+++ web/src/components/PageHistory/PageHistory.tsx
@@ -1,29 +1,24 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import HomeIcon from "../../assets/icons/home.svg?react";
 import ArrowRight from "../../assets/icons/arrow-right.svg?react";
 import styles from "./PageHistory.module.css";
 import { v4 as uuid } from "uuid";
 
 export default function PageHistory() {
-  const links = [{ name: "About", url: "/about" }];
-  const history = links.map((el) => {
-    return {
-      ...el,
-      id: uuid(),
-    };
-  });
+  const { pathname } = useLocation();
+  const pathnames = pathname.split("/").filter((el) => el !== "");
 
   return (
     <header className={styles["page-history"]}>
       <Link to={"/"} className={styles.home}>
         <HomeIcon className={`${styles.icon} icon sm`} />
       </Link>
-      {history.map((link) => {
+      {pathnames.map((link) => {
         return (
           <div className={styles["link-box"]} key={uuid()}>
             <ArrowRight className={`${styles.icon} icon sm`} />
-            <Link to={link.url} className={styles["history-link"]}>
-              {link.name}
+            <Link to={`/${link}`} className={styles["history-link"]}>
+              {link.replace(/-/g, " ")}
             </Link>
           </div>
         );
